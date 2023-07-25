93f62c39f8254fc3b82295d407c06afc3b1c56b6
githukudenis <githukudenis@gmail.com>
2023-07-25 23:13:10 +0300

    Delegate app usage list to SummaryScreen for rendering

--- feature/summary/src/main/java/com/githukudenis/summary/ui/SummaryRoute.kt
+++ feature/summary/src/main/java/com/githukudenis/summary/ui/SummaryRoute.kt
@@ -1,12 +1,18 @@
 package com.githukudenis.summary.ui
 
+import android.app.usage.UsageStats
 import android.content.Intent
 import android.provider.Settings
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,10 +23,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -68,30 +74,7 @@ internal fun SummaryRoute(
                 Lifecycle.Event.ON_START -> {
                     shouldShowPermissionDialog = !context.hasUsageAccessPermissions()
                 }
-
-                Lifecycle.Event.ON_CREATE -> {
-                    Log.d("summary lifecycle", "Created")
-                }
-
-                Lifecycle.Event.ON_RESUME -> {
-                    Log.d("summary lifecycle", "Resumed")
-                }
-
-                Lifecycle.Event.ON_PAUSE -> {
-                    Log.d("summary lifecycle", "Paused")
-                }
-
-                Lifecycle.Event.ON_STOP -> {
-                    Log.d("summary lifecycle", "Stopped")
-                }
-
-                Lifecycle.Event.ON_DESTROY -> {
-                    Log.d("summary lifecycle", "Destroyed")
-                }
-
-                Lifecycle.Event.ON_ANY -> {
-                    Log.d("summary lifecycle", "Any")
-                }
+                else -> Unit
             }
         }
 
@@ -145,36 +128,48 @@ internal fun SummaryRoute(
         )
     }
 
-
-    if (context.hasUsageAccessPermissions()) {
-        val uiState by summaryViewModel.uiState.collectAsStateWithLifecycle()
-        when (val currentState = uiState) {
-            SummaryUiState.Loading -> {
-                Log.d("summary", "Loading")
-            }
-
-            is SummaryUiState.Success -> {
-                Log.d("summary", currentState.summaryData.toString())
-
-            }
-
-            is SummaryUiState.Error -> {
-                Log.d("summary", "Error")
-            }
+    val uiState by summaryViewModel.uiState.collectAsStateWithLifecycle()
+    when (val currentState = uiState) {
+        SummaryUiState.Loading -> {
+            Log.d("summary", "Loading")
         }
-        SummaryScreen()
-    } else {
-        Text(
-            text = context.getString(R.string.permission_dialog_description)
-        )
+
+        is SummaryUiState.Success -> {
+            val stats = currentState.summaryData.usageStats.asSequence()
+            val appUsageList = stats.map {
+                it.toPair()
+            }
+                .toList()
+
+            SummaryScreen(usageStats = appUsageList)
+        }
+
+        is SummaryUiState.Error -> {
+            Log.d("summary", "Error")
+        }
     }
+
 }
 
 @Composable
-internal fun SummaryScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "Summary"
-        )
+internal fun SummaryScreen(
+    usageStats: List<Pair<String, UsageStats>>
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(items = usageStats) {
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = it.first
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Text(
+                        text = it.second.totalTimeInForeground.toString()
+                    )
+                }
+            }
+        }
     }
 }
