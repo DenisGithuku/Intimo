5ca62f7e5d7c43cc58f1cde3f771deab1bcb1d1c
githukudenis <githukudenis@gmail.com>
2023-08-11 01:39:44 +0300

    Move value on top

--- feature/summary/src/main/java/com/githukudenis/summary/ui/components/CardInfo.kt
+++ feature/summary/src/main/java/com/githukudenis/summary/ui/components/CardInfo.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -21,14 +22,16 @@ fun CardInfo(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = title,
-            style = MaterialTheme.typography.bodySmall,
+            text = value,
+            style = MaterialTheme.typography.headlineLarge.copy(
+                fontWeight = FontWeight.Bold
+            ),
             textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
-            text = value,
-            style = MaterialTheme.typography.labelMedium,
+            text = title,
+            style = MaterialTheme.typography.bodyMedium,
             textAlign = TextAlign.Center
         )
     }
