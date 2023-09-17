3c43494705df622df6aa637791e25986b83ec7c9
githukudenis <githukudenis@gmail.com>
2023-09-17 22:29:13 +0300

    refactor layout to use lazy row.

--- feature/summary/src/main/java/com/githukudenis/intimo/feature/summary/ui/components/HabitCard.kt
+++ feature/summary/src/main/java/com/githukudenis/intimo/feature/summary/ui/components/HabitCard.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
